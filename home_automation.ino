53768f2a6573b02dd9168481099fd54bf9fb2d3b
OlusojiSalome001 <62818835+OlusojiSalome001@users.noreply.github.com>
2023-03-12 14:59:51 +0100

    Add files via upload

---
+++ home_automation.ino
@@ -0,0 +1,29 @@
+
+char bluetoothVal;//converting data received from serial monitor to integer
+int relaySwitch = 8;
+bool val = HIGH;
+void setup() {
+  Serial.begin(9600);//enable serial monitor
+  pinMode(relaySwitch, OUTPUT);//define relay pin
+
+}
+
+void loop() {
+  if (Serial.available() == 0) {
+  }
+   bluetoothVal = Serial.read();//read serial value
+  Serial.println(bluetoothVal);
+  if (bluetoothVal == '1') {
+    val = HIGH;
+    digitalWrite(relaySwitch, val);//turn on the bulb
+    
+  } 
+  else if (bluetoothVal == '0') {
+    val = LOW;
+    digitalWrite(bluetoothVal, val);//turn off the bulb
+  }
+  else{
+    digitalWrite(relaySwitch,val);//leave the bulb the way it is
+  } 
+ delay(50);
+}
